6c906dbc42564305df23798e399a45bd706feec7
Tania Ramon <tania-ramon-prats@hotmail.com>
2016-10-14 19:25:13 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*1. Escriure un programa en llenguatge Java que mostri per pantalla els números del 0 al
+        1000.*/
+
+        for (int i = 0; i<=1000; i++ ){
+
+            System.out.println("Número " + i);
+        }
+
+    }
+}
